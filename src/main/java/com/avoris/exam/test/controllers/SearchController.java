f11d9bfcb280232970182460ae186f26630051c5
= <raul.gutialo@gmail.com>
2023-05-26 10:51:07 +0200

    Working in controller

--- src/main/java/com/avoris/exam/test/controllers/SearchController.java
+++ src/main/java/com/avoris/exam/test/controllers/SearchController.java
@@ -1,6 +1,12 @@
 package com.avoris.exam.test.controllers;
 
+import com.avoris.exam.test.model.Search;
+import com.avoris.exam.test.model.SearchDetails;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +15,12 @@ public class SearchController {
     @GetMapping("/hello-world")
     public String helloWorld(){
         return "Hello World";
+    }
+
+    @PostMapping("/search")
+    public ResponseEntity<Search> addSearch(@RequestBody @NonNull SearchDetails searchDetails){
+
+        System.out.println(searchDetails.toString());
+        return ResponseEntity.created(null).build();
     }
 }
